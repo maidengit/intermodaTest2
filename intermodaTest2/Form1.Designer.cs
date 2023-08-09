49cf1bac8b55b828321daec0cb37b44e1555fffb
maiden <adonay22@outlook.com>
2023-08-09 12:32:11 -0600

    Interfaz creada

---
+++ intermodaTest2/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace intermodaTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btAgregar = new System.Windows.Forms.Button();
+            this.btEditar = new System.Windows.Forms.Button();
+            this.btEliminar = new System.Windows.Forms.Button();
+            this.btRenovar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(52, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(655, 221);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btAgregar
+            // 
+            this.btAgregar.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btAgregar.Location = new System.Drawing.Point(145, 70);
+            this.btAgregar.Name = "btAgregar";
+            this.btAgregar.Size = new System.Drawing.Size(136, 39);
+            this.btAgregar.TabIndex = 1;
+            this.btAgregar.Text = "Agregar";
+            this.btAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btEditar
+            // 
+            this.btEditar.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btEditar.Location = new System.Drawing.Point(287, 70);
+            this.btEditar.Name = "btEditar";
+            this.btEditar.Size = new System.Drawing.Size(136, 39);
+            this.btEditar.TabIndex = 2;
+            this.btEditar.Text = "Editar";
+            this.btEditar.UseVisualStyleBackColor = true;
+            // 
+            // btEliminar
+            // 
+            this.btEliminar.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btEliminar.Location = new System.Drawing.Point(429, 70);
+            this.btEliminar.Name = "btEliminar";
+            this.btEliminar.Size = new System.Drawing.Size(136, 39);
+            this.btEliminar.TabIndex = 3;
+            this.btEliminar.Text = "Eliminar";
+            this.btEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btRenovar
+            // 
+            this.btRenovar.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btRenovar.Location = new System.Drawing.Point(571, 70);
+            this.btRenovar.Name = "btRenovar";
+            this.btRenovar.Size = new System.Drawing.Size(136, 39);
+            this.btRenovar.TabIndex = 4;
+            this.btRenovar.Text = "Renovar";
+            this.btRenovar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 380);
+            this.Controls.Add(this.btRenovar);
+            this.Controls.Add(this.btEliminar);
+            this.Controls.Add(this.btEditar);
+            this.Controls.Add(this.btAgregar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button btAgregar;
+        private Button btEditar;
+        private Button btEliminar;
+        private Button btRenovar;
+    }
+}
